444fa0e6fa99822887cf264dfacae985b4cd6f03
Rauulll <nyaokeraul003@gmail.com>
2024-06-14 15:40:14 +0300

    picking-color-codes

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -2,6 +2,7 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
   end
 
   def hash_input(input) do
@@ -9,5 +10,11 @@ defmodule Identicon do
     |> :binary.bin_to_list()
 
     %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
+
+    [r, g, b]
   end
 end
