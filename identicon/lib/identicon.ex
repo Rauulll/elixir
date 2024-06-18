635a54ce050f2cb369178f493ea0b1afde9bbceb
Rauulll <nyaokeraul003@gmail.com>
2024-06-18 22:30:01 +0300

    building-grid

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,6 +3,7 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
   def hash_input(input) do
@@ -12,9 +13,18 @@ defmodule Identicon do
     %Identicon.Image{hex: hex}
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail] } = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
 
-    [r, g, b]
+  def build_grid(%Identicon.Image{hex: hex} = _image) do
+    hex
+    |> Enum.chunk_every(3, 3, :discard)
+    |> Enum.map( fn x -> Identicon.mirror_row(x) end)
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
   end
 end
