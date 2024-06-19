9d3a1e12d10fb3ae9d007897c56d2c2572f59ffa
Rauulll <nyaokeraul003@gmail.com>
2024-06-19 18:41:00 +0300

    Completing pixel-generated-image

--- identicon/README.md
+++ identicon/README.md
@@ -4,6 +4,17 @@
 
 ## Installation
 
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `cards` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:identicon, "~> 0.1.0"}
+  ]
+end
+```
+
 ```
 ```
 ```
--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -4,6 +4,10 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
 
   def hash_input(input) do
@@ -17,14 +21,55 @@ defmodule Identicon do
     %Identicon.Image{image | color: {r, g, b}}
   end
 
-  def build_grid(%Identicon.Image{hex: hex} = _image) do
-    hex
-    |> Enum.chunk_every(3, 3, :discard)
-    |> Enum.map( fn x -> Identicon.mirror_row(x) end)
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
     [first, second | _tail] = row
     row ++ [second, first]
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter(grid, fn ({code, _index}) ->
+      rem(code, 2) == 0
+    end)
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map(grid, fn ({_code, index}) ->
+      horizontol = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontol, vertical}
+      bottom_left = {horizontol + 50, vertical + 50}
+
+      {top_left, bottom_left}
+    end)
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each(pixel_map, fn ({top_left, bottom_left}) ->
+      :egd.filledRectangle(image, top_left, bottom_left, fill)
+    end)
+
+    :egd.render(image)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
   end
 end
